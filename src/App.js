0691da25be47baf0cd79247509bf168e063105ff
Nathan Shepherd <your@email.goes.here>
2020-05-24 18:14:32 +0200

    App component set-up (framework: Material-UI)
    - Material-UI theme set-up with ThemeProvider context provider
    - "prefers-color-scheme" media query support for user preference
    dark/light themes
    - component tree set-up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo } from 'react';
+// Components
+import Body from './containers/Body';
+import Header from './presentational/Header';
+import Wrapper from './presentational/Wrapper';
+// Theme
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
+
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = useMemo(
+      () =>
+          createMuiTheme({
+              palette: {
+                  type: prefersDarkMode ? 'dark' : 'light',
+                  primary: {
+                      main: '#aa1111',
+                  },
+              },
+          }),
+      [prefersDarkMode],
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Body>
+        <Header />
+        <Wrapper />
+      </Body>
+    </ThemeProvider>
   );
 }
 
