f19d72153471bb08bcb4a062cf26ce149624eb68
Nathan Shepherd <your@email.goes.here>
2020-05-26 00:06:55 +0200

    UI update for improved feedback

--- src/presentational/Results.js
+++ src/presentational/Results.js
@@ -13,6 +13,9 @@ const useStyles = makeStyles(theme => ({
     },
     typography: {
         fontWeight: 300,
+        '& small': {
+            fontSize: 12,
+        }
     },
     divider: {
         margin: theme.spacing(2, 0),
@@ -32,7 +35,8 @@ const Results = (props) => {
                 <Box flexGrow={1}>
                     {EarliestPermissibleDebitOrderStartMonth && (
                         <Typography className={classes.typography} variant="h6" color="textSecondary">Debit Order Start Month: &nbsp;
-                            <Typography display="inline" color="textPrimary">{EarliestPermissibleDebitOrderStartMonth}</Typography>
+                            <Typography display="inline" color="textPrimary">{EarliestPermissibleDebitOrderStartMonth}</Typography> &nbsp;
+                            <Typography display="inline" color="textSecondary"><sup>(2,500 pm)</sup></Typography>
                         </Typography>
                     )}
                 </Box>
@@ -41,10 +45,11 @@ const Results = (props) => {
                     {TotalContributions && (
                         <Typography className={classes.typography} variant="h6" color="textSecondary">Total Contributions: &nbsp;
                             <Typography display="inline" color="textPrimary">{TotalContributions}</Typography>
-                            {ExcessContributions && (
+                            {(Number(ExcessContributions) > 0) && (
                                 <Typography className={classes.typography} variant="subtitle1">
                                     Your contributions are in excess of: &nbsp;
-                                    <Typography display="inline" color="error">{ExcessContributions}</Typography>
+                                    <Typography display="inline" color="error">{ExcessContributions}</Typography> *<br/>
+                                    <Typography display="inline" component="small" color="textSecondary">* Consider adjusting your first debit month to the suggested start month above or reducing the investment amount</Typography>
                                 </Typography>
                             )}
                         </Typography>
