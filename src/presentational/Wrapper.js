02d359f0ed166ed1ca8979495d05c64b52480193
Nathan Shepherd <your@email.goes.here>
2020-05-25 20:27:25 +0200

    error handling: error boundary wrapper added for child component render tree

--- src/presentational/Wrapper.js
+++ src/presentational/Wrapper.js
@@ -1,5 +1,6 @@
 import React from 'react';
 // Components
+import WrapperErrorBoundary from '../error-boundaries/WrapperErrorBoundary';
 import Form from '../containers/Form';
 // Theme
 import Grid from '@material-ui/core/Grid';
@@ -7,11 +8,13 @@ import Grid from '@material-ui/core/Grid';
 const Wrapper = () => {
 
     return (
-        <Grid container alignItems="center" justify="center">
-            <Grid item xs={10} sm={8} md={8} lg={8} xl={8}>
-                <Form />
+        <WrapperErrorBoundary>
+            <Grid container alignItems="center" justify="center">
+                <Grid item xs={10} sm={8} md={8} lg={8} xl={8}>
+                    <Form />
+                </Grid>
             </Grid>
-        </Grid>
+        </WrapperErrorBoundary>
     );
 }
 
