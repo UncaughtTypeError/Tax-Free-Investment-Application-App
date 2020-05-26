dc432d652c81c1ea2debc6d801dec9a0bdf312ff
Nathan Shepherd <your@email.goes.here>
2020-05-26 23:37:01 +0200

    JSDoc documentation added

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -1,7 +1,26 @@
+/**
+ * A number, or a string containing a number.
+ * @typedef {(number|string)} NumberLike
+ */
+
+/**
+ * Formats input to float
+ * @param {NumberLike} input - the number or string to convert to a float
+ * @returns {string} A float
+ */
 function formatNumberDecimal(input) {
     return Number.parseFloat(input).toFixed(2);
 }
 
+/**
+ * Formats input to integer
+ * @param {Object} object - the object containing numbers or strings to convert to integers
+ * @param {NumberLike} object.LumpSumInvestmentMonth - the chosen month of the lump sum investment
+ * @param {NumberLike} object.LumpSumInvestmentAmount - the specified lump sum investment amount
+ * @param {NumberLike} object.DebitOrderStartMonth - the chosen month of the first debit order
+ * @param {NumberLike} object.DebitOrderAmount - the predefined monthly deposit amount
+ * @returns {Object} Converted value types
+ */
 function formatNumbers(object) {
     for(let [key, value] of Object.entries(object)) {
         value = Number(value);
@@ -10,10 +29,23 @@ function formatNumbers(object) {
     return object;
 }
 
+/**
+ * Formats input to integer
+ * @param {Object} applicationParameters - the object containing numbers or strings to convert to integers
+ * @param {NumberLike} applicationParameters.LumpSumInvestmentMonth - the chosen month of the lump sum investment
+ * @param {NumberLike} applicationParameters.LumpSumInvestmentAmount - the specified lump sum investment amount
+ * @param {NumberLike} applicationParameters.DebitOrderStartMonth - the chosen month of the first debit order
+ * @param {NumberLike} applicationParameters.DebitOrderAmount - the predefined monthly deposit amount
+ * @returns {Object} EarliestPermissibleDebitOrderStartMonth that first debit order may occur, TotalContributions of lump sum and debit orders, ExcessContributions exceeding tax year cap
+ * @todo Allow variable debit amount input.
+ */
 export default function applicationCheck(applicationParameters) {
 
     let { LumpSumInvestmentMonth = 0, LumpSumInvestmentAmount = 0, DebitOrderStartMonth = 0, DebitOrderAmount = 0 } = formatNumbers(applicationParameters);
 
+    /** @constant {number} 
+     *  @default
+     */
     const LUMP_SUM_LIMIT = 30000;
 
     let EarliestPermissibleDebitOrderStartMonth,
@@ -29,6 +61,11 @@ export default function applicationCheck(applicationParameters) {
         ExcessContributions: 0,
     }
 
+    /**
+     * Calculates debit start month
+     * @param {number} CalculateMonth - an integer representing a month in a 14 month cycle
+     * @returns {number} integer representing month of first debit order
+     */
     const calculateStartMonth = (CalculateMonth) => {
         switch(CalculateMonth) {
             case 13: // Jan
@@ -38,14 +75,23 @@ export default function applicationCheck(applicationParameters) {
             default:
                 return CalculateMonth;
         }
-
     }
 
+    /**
+     * Sets debit start month
+     * @description determine if lump sum amount is provided and if lump sum month is greater than debit month, then select the applicable start month reference
+     * @summary determine applicable start month reference (uses scoped state)
+     */
     const setStartMonth = () => {
         let CalculateMonth = (LumpSumInvestmentAmount > 0 && LumpSumInvestmentMonth >= DebitOrderStartMonth) ? (LumpSumInvestmentMonth + 1) : DebitOrderStartMonth;
         scopeState.StartMonth = calculateStartMonth(CalculateMonth);
     }
 
+    /**
+     * Sets remaining months of the year
+     * @description determine which side of the tax year application falls on, then select the applicable total to deduct start month from
+     * @summary determine which side of the tax year to deduct start month from (uses scoped state)
+     */
     const setMonthsRemaining = () => {
         let { StartMonth, MonthsRemaining } = scopeState;
 
@@ -57,16 +103,31 @@ export default function applicationCheck(applicationParameters) {
         scopeState.MonthsRemaining = MonthsRemaining;
     }
 
+    /**
+     * Sets maximum amount that may be debited
+     * @description determine maximum debit contributions by multiplying the monthly debit amount by the remaining months
+     * @summary determine maximum amount that may be debited in remaining tax year (uses scoped state)
+     */
     const setMaximumDebitAmount = () => {
         let { MonthsRemaining } = scopeState;
         scopeState.MaximumDebitAmount = MonthsRemaining * DebitOrderAmount;
     }
 
+    /**
+     * Sets maximum total contributions
+     * @description determine maximum total contributions by summing the maximum debit amount possible with any provided lump sum investment
+     * @summary determine maximum total contributions for the remaining tax year (uses scoped state)
+     */
     const setMaximumTotal = () => {
         let { MaximumDebitAmount } = scopeState;
         scopeState.MaximumTotal = MaximumDebitAmount + LumpSumInvestmentAmount;
     }
 
+    /**
+     * Sets earliest possible debit month
+     * @description determine if any debit months are possible and roll over to first debit month of the follow tax year, else determine the earliest possible debit month by deducting the maximum debit months possible from the remaining months of the tax year. Set excess contributions from the difference of the maximum total contributions and tax year contributions cap.
+     * @summary determine the earliest possible debit month if in excess of contributions tax cap (uses scoped state)
+     */
     const setEarliestStartMonth = () => {
         let { MaximumDebitMonths, StartMonth, MonthsRemaining } = scopeState;
 
